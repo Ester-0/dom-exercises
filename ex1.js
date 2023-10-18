87608743285b50a7d852252216cb6cb756a9bd63
Ester-0 <azalea_@hotmail.com>
2023-10-18 13:09:59 +0200

    ok

--- ex1.js
+++ ex1.js
@@ -9,27 +9,50 @@
  * Cambia el título de la página a rojo usando un estilo en línea
  */
 
+let h1 = document.querySelector('h1').style.color = "red"; document.querySelector('h1').style.fontSize = "48px";
+
+
+
 /**
  * Cambia el tamaño del título de la página a 48px usando un estilo en línea
  */
+
+    // hecho junto al apartado anterior
 
 /**
  * Muestra por console.log el texto de la caja 2
  */
 
+let caja = document.querySelector('#caja-2').textContent;
+
+console.log(caja);
+
 /**
  * Cambia el color del borde del párrafo p.intro a verde usando un estilo en línea
  */
+
+let intro = document.querySelector('.intro').style.border = "solid green 20px";
+
+
 
 /**
  * Elimina la clase "estilo-3" de la última caja.
  * Ayuda: https://www.w3schools.com/howto/howto_js_remove_class.asp
  */
 
+let ultima = document.getElementById('ultimo');
+            ultima.classList.remove("estilo-3")
+
+
+
 /**
  * Añade la clase CSS "ocultar" al penúltimo párrafo
- * Ayuda: https://simpledev.io/lesson/add-class-js/
+ * Ayuda: https://simpledev.io/lesson/add-class-js/ 
+ *        https://www.w3schools.com/cssref/sel_nth-of-type.php (usando esto no hace falta añadir una id al html)
  * 
  * BONUS: Haz el ejercicio sin modificar el ex1.html
- 
  */
+  
+let newCSS = document.querySelector('p:nth-of-type(6)');
+newCSS.classList.add('ocultar');
+
